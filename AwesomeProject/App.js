64f8d44b0fc5e2b609baaeb2293d5663a8e7d794
Wicrenet_Jason <xul2@wicrenet.com>
2018-02-24 15:54:10 +0800

    简单的页面布局

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -7,10 +7,15 @@
 import React, { Component } from 'react';
 import {
   Platform,
+  Image,
   StyleSheet,
   Text,
   View
 } from 'react-native';
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Jason', year: '2018', posters: {thumbnail: 'https://tvax2.sinaimg.cn/crop.0.0.929.929.180/a4706cc9ly8fopiy536i7j20pt0pt0tp.jpg'}},
+];
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -21,38 +26,42 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
+    render() {
+    var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail} />
+       <View style={styles.rightContainer}>
+            <Text style={styles.title}>{movie.title}</Text>
+            <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+var styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  thumbnail: {
+    width: 60,
+    height: 60,
   },
-  instructions: {
+
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  },
+  year: {
+    textAlign: 'center',
   },
 });
